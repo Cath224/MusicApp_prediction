393fa576038a61f80d6adfa4a705617554d1a468
Ekaterina Yakovenko <e.yakovenko@itfbgroup.ru>
2024-05-28 13:50:10 +0300

    fix

--- src/main/java/ru/musicapp/predictionservice/PredictionServiceApplication.java
+++ src/main/java/ru/musicapp/predictionservice/PredictionServiceApplication.java
@@ -2,8 +2,11 @@ package ru.musicapp.predictionservice.predictionservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.flyway.FlywayAutoConfiguration;
+import org.springframework.context.annotation.Import;
 
 @SpringBootApplication
+@Import(FlywayAutoConfiguration.class)
 public class PredictionServiceApplication {
 
     public static void main(String[] args) {
